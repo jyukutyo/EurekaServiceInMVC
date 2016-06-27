db591bd0732ab59d74430341fab78a5299900d66
Koichi Sakata <sakata.koichi@furyu.jp>
2016-06-27 16:34:51 +0900

    コードをシンプルにした

--- client/src/main/java/com/jyukutyo/AppConfig.java
+++ client/src/main/java/com/jyukutyo/AppConfig.java
@@ -1,14 +1,21 @@
 package com.jyukutyo;
 
+import java.io.IOException;
+
 import com.netflix.appinfo.ApplicationInfoManager;
 import com.netflix.appinfo.EurekaInstanceConfig;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.appinfo.MyDataCenterInstanceConfig;
 import com.netflix.appinfo.providers.EurekaConfigBasedInstanceInfoProvider;
+import com.netflix.client.config.IClientConfig;
+import com.netflix.config.ConfigurationManager;
 import com.netflix.config.DynamicPropertyFactory;
 import com.netflix.discovery.DefaultEurekaClientConfig;
 import com.netflix.discovery.DiscoveryClient;
 import com.netflix.discovery.EurekaClient;
+import com.netflix.ribbon.transport.netty.RibbonTransport;
+import com.netflix.ribbon.transport.netty.http.LoadBalancingHttpClient;
+import io.netty.buffer.ByteBuf;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -34,6 +41,15 @@ public class AppConfig {
     @Bean
     public DynamicPropertyFactory dynamicPropertyFactory() {
         return DynamicPropertyFactory.getInstance();
+    }
+
+    @Bean
+    public LoadBalancingHttpClient<ByteBuf, ByteBuf> loadBalancingHttpClient() throws IOException {
+        ConfigurationManager.loadPropertiesFromResources("eureka.properties");
+
+        IClientConfig clientConfig = IClientConfig.Builder.newBuilder("sample-client").build();
+        LoadBalancingHttpClient<ByteBuf, ByteBuf> ribbonClient = RibbonTransport.newHttpClient(clientConfig);
+        return ribbonClient;
     }
 
 }
--- client/src/main/java/com/jyukutyo/EurekaClientController.java
+++ client/src/main/java/com/jyukutyo/EurekaClientController.java
@@ -2,17 +2,6 @@ package com.jyukutyo;
 
 import java.nio.charset.Charset;
 
-import com.netflix.client.config.IClientConfig;
-import com.netflix.config.ConfigurationManager;
-import com.netflix.discovery.EurekaClient;
-import com.netflix.loadbalancer.ILoadBalancer;
-import com.netflix.loadbalancer.RandomRule;
-import com.netflix.loadbalancer.ZoneAwareLoadBalancer;
-import com.netflix.niws.loadbalancer.DefaultNIWSServerListFilter;
-import com.netflix.niws.loadbalancer.DiscoveryEnabledNIWSServerList;
-import com.netflix.niws.loadbalancer.EurekaNotificationServerListUpdater;
-import com.netflix.niws.loadbalancer.NIWSDiscoveryPing;
-import com.netflix.ribbon.transport.netty.RibbonTransport;
 import com.netflix.ribbon.transport.netty.http.LoadBalancingHttpClient;
 import io.netty.buffer.ByteBuf;
 import io.reactivex.netty.protocol.http.client.HttpClientRequest;
@@ -27,18 +16,11 @@ import rx.Observable;
 public class EurekaClientController {
 
     @Autowired
-    EurekaClient eurekaClient;
+    LoadBalancingHttpClient<ByteBuf, ByteBuf> ribbonClient;
 
-    @RequestMapping("/example")
+    @RequestMapping("/")
     @ResponseBody
     public String example() throws Exception {
-        ConfigurationManager.loadPropertiesFromResources("eureka.properties");
-
-        IClientConfig clientConfig = IClientConfig.Builder.newBuilder("sample-client").build();
-        DiscoveryEnabledNIWSServerList serverList = new DiscoveryEnabledNIWSServerList(clientConfig, () -> eurekaClient);
-        ILoadBalancer loadBalancer = new ZoneAwareLoadBalancer(clientConfig, new RandomRule(), new NIWSDiscoveryPing(), serverList, new DefaultNIWSServerListFilter(), new EurekaNotificationServerListUpdater());
-
-        LoadBalancingHttpClient<ByteBuf, ByteBuf> ribbonClient = RibbonTransport.newHttpClient(loadBalancer);
         HttpClientRequest<ByteBuf> request = HttpClientRequest.createGet("/jyukutyo");
 
         Observable<HttpClientResponse<ByteBuf>> observable = ribbonClient.submit(request);
