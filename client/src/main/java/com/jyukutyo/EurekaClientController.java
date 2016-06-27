4018680e670f5f2c768e1a2a963970f4c5941ed4
Koichi Sakata <sakata.koichi@furyu.jp>
2016-06-27 16:24:14 +0900

    RestClientからnettyでのアクセスに変更した。
    RestClientがdeprecatedなため。

--- client/src/main/java/com/jyukutyo/EurekaClientController.java
+++ client/src/main/java/com/jyukutyo/EurekaClientController.java
@@ -1,36 +1,48 @@
 package com.jyukutyo;
 
-import java.net.URI;
+import java.nio.charset.Charset;
 
-import com.netflix.client.ClientFactory;
-import com.netflix.client.http.HttpRequest;
-import com.netflix.client.http.HttpResponse;
+import com.netflix.client.config.IClientConfig;
 import com.netflix.config.ConfigurationManager;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.loadbalancer.RandomRule;
 import com.netflix.loadbalancer.ZoneAwareLoadBalancer;
-import com.netflix.niws.client.http.RestClient;
-import org.apache.commons.io.IOUtils;
+import com.netflix.niws.loadbalancer.DefaultNIWSServerListFilter;
+import com.netflix.niws.loadbalancer.DiscoveryEnabledNIWSServerList;
+import com.netflix.niws.loadbalancer.EurekaNotificationServerListUpdater;
+import com.netflix.niws.loadbalancer.NIWSDiscoveryPing;
+import com.netflix.ribbon.transport.netty.RibbonTransport;
+import com.netflix.ribbon.transport.netty.http.LoadBalancingHttpClient;
+import io.netty.buffer.ByteBuf;
+import io.reactivex.netty.protocol.http.client.HttpClientRequest;
+import io.reactivex.netty.protocol.http.client.HttpClientResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import rx.Observable;
 
 @Controller
 public class EurekaClientController {
 
-    @RequestMapping("/")
+    @Autowired
+    EurekaClient eurekaClient;
+
+    @RequestMapping("/example")
     @ResponseBody
     public String example() throws Exception {
-        ConfigurationManager.loadPropertiesFromResources("eureka.properties");  // 1
-        RestClient client = (RestClient) ClientFactory.getNamedClient("sample-client");  // 2
+        ConfigurationManager.loadPropertiesFromResources("eureka.properties");
 
-        HttpRequest request = HttpRequest.newBuilder().uri(new URI("/jyukutyo")).build(); // 3
-        for (int i = 0; i < 20; i++)  {
-            HttpResponse response = client.executeWithLoadBalancer(request); // 4
-            System.out.println("Status code for " + response.getRequestedURI() + "  :" + response.getStatus() + " : " + IOUtils.toString(response.getInputStream(), "UTF-8"));
-        }
-        @SuppressWarnings("rawtypes")
-        ZoneAwareLoadBalancer lb2 = (ZoneAwareLoadBalancer) client.getLoadBalancer();
-        System.out.println(lb2.getLoadBalancerStats());
-        return "example";
+        IClientConfig clientConfig = IClientConfig.Builder.newBuilder("sample-client").build();
+        DiscoveryEnabledNIWSServerList serverList = new DiscoveryEnabledNIWSServerList(clientConfig, () -> eurekaClient);
+        ILoadBalancer loadBalancer = new ZoneAwareLoadBalancer(clientConfig, new RandomRule(), new NIWSDiscoveryPing(), serverList, new DefaultNIWSServerListFilter(), new EurekaNotificationServerListUpdater());
+
+        LoadBalancingHttpClient<ByteBuf, ByteBuf> ribbonClient = RibbonTransport.newHttpClient(loadBalancer);
+        HttpClientRequest<ByteBuf> request = HttpClientRequest.createGet("/jyukutyo");
+
+        Observable<HttpClientResponse<ByteBuf>> observable = ribbonClient.submit(request);
+        return observable.flatMap(resp -> resp.getContent().map(bb -> bb.toString(Charset.defaultCharset()))).toBlocking().first();
     }
 
 }
--- client/src/main/java/com/jyukutyo/MyInitializingBean.java
+++ client/src/main/java/com/jyukutyo/MyInitializingBean.java
@@ -1,5 +1,9 @@
 package com.jyukutyo;
 
+import com.netflix.appinfo.ApplicationInfoManager;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.config.DynamicPropertyFactory;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -8,10 +12,49 @@ import org.springframework.stereotype.Component;
 public class MyInitializingBean implements InitializingBean {
 
     @Autowired
-    ExampleServiceBase exampleServiceBase;
+    ApplicationInfoManager applicationInfoManager;
+    @Autowired
+    EurekaClient eurekaClient;
+    @Autowired
+    DynamicPropertyFactory configInstance;
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        exampleServiceBase.start();
+        // A good practice is to register as STARTING and only change status to UP
+        // after the service is ready to receive traffic
+        System.out.println("Registering service to eureka with STARTING status");
+        applicationInfoManager.setInstanceStatus(InstanceInfo.InstanceStatus.STARTING);
+
+        System.out.println("Simulating service initialization by sleeping for 2 seconds...");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            // Nothing
+        }
+
+        // Now we change our status to UP
+        System.out.println("Done sleeping, now changing status to UP");
+        applicationInfoManager.setInstanceStatus(InstanceInfo.InstanceStatus.UP);
+        waitForRegistrationWithEureka(eurekaClient);
+        System.out.println("Service started and ready to process requests..");
+    }
+
+    private void waitForRegistrationWithEureka(EurekaClient eurekaClient) {
+        // my vip address to listen on
+        String vipAddress = configInstance.getStringProperty("eureka.vipAddress", "example.com").get();
+        InstanceInfo nextServerInfo = null;
+        while (nextServerInfo == null) {
+            try {
+                nextServerInfo = eurekaClient.getNextServerFromEureka(vipAddress, false);
+            } catch (Throwable e) {
+                System.out.println("Waiting ... verifying service registration with eureka ...");
+
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
     }
 }
